03627add7be1ea6a87462bf9194cfca50878b9e8
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-26 16:57:28 +0200

    js: base function and active logic carousel

--- js/script.js
+++ js/script.js
@@ -26,3 +26,80 @@ const images = [
       text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
     },
 ];
+
+//* Funzione per creare literal templates per le varie immagini
+function creationItems(image) {
+    container.innerHTML += `
+    <div class="d-none ${image.active}">
+      <div>
+        <img src="./${image.image}" alt="" />
+      </div>
+      <h1>${image.title}</h1>
+      <p>${image.text}</p>
+    </div> `;
+}
+
+//* Funzione per creare nuovo oggetti
+function printImg() {
+    const newArrayImages = images.map((image, index) => {
+      imagesCarousel = {
+        image: image.image,
+        title: image.title,
+        text: image.text,
+        active: index == select ? "active" : "",
+      };
+      creationItems(imagesCarousel);
+      return imagesCarousel;
+    });
+}
+  
+//* Funzione per i bottoni next e bottom
+function nextPrevButton(incrementoDecremento) {
+    //* Recupero tutte le slides
+    const allSlides = document.querySelectorAll(".d-none");
+  
+    //* Recupero la slide attiva e vado a rimuovere la classe active
+    const activeSlide = allSlides[incrementoDecremento];
+    activeSlide.classList.toggle("active");
+  
+    incrementoDecremento;
+    //* se superata la lunghezza dell'array torniamo alla prima slide corrispondente in base alla direzione
+    if (select >= allSlides.length) {
+        select = 0;
+      }
+      if (select < 0){
+        select = allSlides.length - 1;
+    } 
+      
+    //* Recuperp la nuova slide attiva e vado ad aggiungere la classe active
+    const newActiveSlide = allSlides[select];
+    newActiveSlide.classList.toggle("active");
+}
+
+
+//* Recupero elementi dal DOM
+
+//* Recupero il container dal DOM
+const container = document.getElementById("container-image");
+//* Recupero tasto Next dal DOM
+const next = document.getElementById("next");
+//* Recupero tasto Prev dal DOM
+const prev = document.getElementById("prev");
+
+//* Inizializzo la variabile select
+let select = 0;
+
+//*invoco la funzione printImg
+printImg();
+
+
+//* Evento tasto Next
+next.addEventListener("click", function () {
+  nextPrevButton(select--);
+});
+
+//* Evento tasto Prev
+prev.addEventListener("click", function () {
+  nextPrevButton(select++);
+});
+  
